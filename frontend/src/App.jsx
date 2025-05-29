eded28e9a7b598c8b87333bed308b6db01f3a0ce
PeyalaAnandanaidu <anandanaidupeyala@gmail.com>
2025-05-29 17:35:39 +0530

    Navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home/Home'
+import Cart from './pages/Cart/Cart'
+import placeOrder from './pages/placeOrder/placeOrder'
 
 const App = () => {
   return (
-    <div>
-      
+    <div className='app'>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/cart' element={<Cart/>} />
+        <Route path='/placeorder' element={<placeOrder/>} />
+      </Routes>
     </div>
   )
 }
---
+++ frontend/src/pages/placeOrder/placeOrder.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './placeOrder.css'
+
+
+const placeOrder = () => {
+  return (
+    <div>placeOrder</div>
+  )
+}
+
+export default placeOrder
