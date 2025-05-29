8bed670b2ea68e5637159f1fc823aa262f5847ec
PeyalaAnandanaidu <anandanaidupeyala@gmail.com>
2025-05-29 21:11:19 +0530

    header

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import './Home.css'
 import Header from '../../components/Header/Header'
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
-import Navbar from '../../components/Navbar/Navbar'
+
 
 const Home = () => {
   return (
     <div>
-      <Navbar/>
+      
       <Header />
       <ExploreMenu/>
     </div>
