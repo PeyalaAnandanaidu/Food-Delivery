b04ceea7b6f8acc484dc83086e6b51f38bb0dd59
PeyalaAnandanaidu <anandanaidupeyala@gmail.com>
2025-05-30 14:08:27 +0530

    dishes

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,16 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Home.css'
 import Header from '../../components/Header/Header'
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
 import Navbar from '../../components/Navbar/Navbar'
+import FoodDisplay from '../../components/FoodDisplay/FoodDisplay'
 
 
 const Home = () => {
+  
+    const [category,setCategory]=useState("All");
+
   return (
     <div>
       <Navbar/>
       <Header />
-      <ExploreMenu/>
+      <ExploreMenu category={category} setCategory={setCategory}/>
+      <FoodDisplay category={category}/>
     </div>
   )
 }
